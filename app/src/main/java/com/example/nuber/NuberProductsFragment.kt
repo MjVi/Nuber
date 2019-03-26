df83f85e97e92dadcc2f50faf977fe6d7afda182
MjVi <ma.jose.villagomez@gmail.com>
2019-03-26 12:57:57 -0600

    V1

--- app/src/main/java/com/example/nuber/NuberProductsFragment.kt
+++ app/src/main/java/com/example/nuber/NuberProductsFragment.kt
@@ -7,6 +7,9 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_nuber_products.*
 
 
@@ -41,9 +44,25 @@ class NuberProductsFragment : Fragment() {
 
         list_recycler_view.apply {
             layoutManager = LinearLayoutManager(activity)
-            adapter = NuberProductsAdapter(salads)
+            adapter = NuberProductsAdapter(mutableListOf<Salad>())
         }
+        getProducts()
     }
 
+    private fun  getProducts(){
+        val refFirebase = FirebaseDatabase.getInstance().getReference("/Salands")
+        refFirebase.addValueEventListener(object: ValueEventListener{
+            override fun onCancelled(p0: DatabaseError) {
+            }
 
+            override fun onDataChange(p0: DataSnapshot) {
+                val lista = mutableListOf<Salad>()
+                p0.children.forEach{
+                    val producto = it.getValue(Salad::class.java)
+                    lista.add(producto!!)
+                }
+                list_recycler_view.adapter = NuberProductsAdapter(lista)
+            }
+        })
+    }
 }
